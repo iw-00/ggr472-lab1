d1311cdc0ed41f9248d790d273d859dada441851
ywiris0 <irisy.wu@mail.utoronto.ca>
2025-01-23 14:55:39 -0500

    readme and comments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ggr472-lab1
- GGR472 Lab 1
+# GGR472 Lab 1
+
+Creating a GeoJSON and HTML webpage.
+
+## Description
+
+The GeoJSON contains stargazing sites in Ontario, adapted from https://www.destinationontario.com/en-ca/articles/best-places-for-stargazing-ontario using geojson.io.
+
+The webpage contains a link to the external website, as well as to the GeoJSON file. There is also a page for trivia and a separate script for its functionality.
--- trivia.js
+++ trivia.js
@@ -2,6 +2,7 @@ const mitchell = document.getElementById("mitchell");
 const brown = document.getElementById("brown");
 const burnell = document.getElementById("burnell");
 const eddington = document.getElementById("eddington");
+const wiki = document.getElementById("wiki");
 
 mitchell.addEventListener("click", () => {
     alert("Nope! Try again.");
@@ -9,11 +10,21 @@ mitchell.addEventListener("click", () => {
 
 brown.addEventListener("click", () => {
     alert("Not quite! Try again.");
+    
 });
 
 burnell.addEventListener("click", () => {
     alert("Woohoo! You got it.");
+    
 });
+
 eddington.addEventListener("click", () => {
     alert("Nope! Try again.");
+});
+
+wiki.addEventListener("click", () => {
+    window.open("https://en.wikipedia.org/wiki/Maria_Mitchell", "_blank");
+    window.open("https://en.wikipedia.org/wiki/Elizabeth_Brown_(astronomer)", "_blank");
+    window.open("https://en.wikipedia.org/wiki/Jocelyn_Bell_Burnell", "_blank");
+    window.open("https://en.wikipedia.org/wiki/Arthur_Eddington", "_blank");
 });
