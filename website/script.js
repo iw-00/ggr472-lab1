da26b67347e74146a6519d1cbdba6c4f42c37ee6
ywiris0 <irisy.wu@mail.utoronto.ca>
2025-01-23 14:17:42 -0500

    created webpage

---
+++ website/script.js
@@ -0,0 +1,6 @@
+const extWeb = document.getElementById("ext-website");
+
+extWeb.addEventListener("click", () => {
+    window.open("https://www.destinationontario.com/en-ca/articles/best-places-for-stargazing-ontario", "_blank");
+});
+
---
+++ website/trivia.js
@@ -0,0 +1,19 @@
+const mitchell = document.getElementById("mitchell");
+const brown = document.getElementById("brown");
+const burnell = document.getElementById("burnell");
+const eddington = document.getElementById("eddington");
+
+mitchell.addEventListener("click", () => {
+    alert("Nope! Try again.");
+});
+
+brown.addEventListener("click", () => {
+    alert("Not quite! Try again.");
+});
+
+burnell.addEventListener("click", () => {
+    alert("Woohoo! You got it.");
+});
+eddington.addEventListener("click", () => {
+    alert("Nope! Try again.");
+});
