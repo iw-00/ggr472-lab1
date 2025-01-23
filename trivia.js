4d233583e361c6cfbb850bc904442a25018094f6
ywiris0 <irisy.wu@mail.utoronto.ca>
2025-01-23 15:13:27 -0500

    update trivia section

--- trivia.js
+++ trivia.js
@@ -2,7 +2,6 @@ const mitchell = document.getElementById("mitchell");
 const brown = document.getElementById("brown");
 const burnell = document.getElementById("burnell");
 const eddington = document.getElementById("eddington");
-const wiki = document.getElementById("wiki");
 
 mitchell.addEventListener("click", () => {
     alert("Nope! Try again.");
@@ -20,11 +19,4 @@ burnell.addEventListener("click", () => {
 
 eddington.addEventListener("click", () => {
     alert("Nope! Try again.");
-});
-
-wiki.addEventListener("click", () => {
-    window.open("https://en.wikipedia.org/wiki/Maria_Mitchell", "_blank");
-    // window.open("https://en.wikipedia.org/wiki/Elizabeth_Brown_(astronomer)", "_blank");
-    // window.open("https://en.wikipedia.org/wiki/Jocelyn_Bell_Burnell", "_blank");
-    // window.open("https://en.wikipedia.org/wiki/Arthur_Eddington", "_blank");
 });
